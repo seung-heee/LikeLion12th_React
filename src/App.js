e30eac78518f556564d7a0adb8be70bd20915e5d
seung-heee <tmdgmldi2595@naver.com>
2024-01-29 12:31:23 +0900

    component 생성, 코드 조각 모음

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Aside from "./component/simantic/Aside";
+import Footer from "./component/simantic/Footer";
+import Header from "./component/simantic/Header";
+import Nav from "./component/simantic/Nav";
+import Section from "./component/simantic/Section";
+
 function App() {
   return (
     <div className="App">
-      Hello React ! 
+      {/* 코드 조각 모음 */}
+      <Header />
+      <Nav />
+      <Section />
+      <Aside />
+      <Footer />
     </div>
   );
 }
