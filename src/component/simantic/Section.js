949a0900f4707ad384d78216de8c3e0184b3c7f8
seung-heee <tmdgmldi2595@naver.com>
2024-01-29 12:36:04 +0900

    props 데이터 전달, 구조 분해 할당

--- src/component/simantic/Section.js
+++ src/component/simantic/Section.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Section = (props) => {
-    console.log(props)
-    
+const Section = ({name, food}) => {
+
     return (
         <>
-            <div>안녕하세요 저는 {props.name}입니다.</div>
-            <div>제가 제일 좋아하는 전은 {props.food}입니다.</div>
+            <div>안녕하세요 저는 {name}입니다.</div>
+            <div>제가 제일 좋아하는 전은 {food}입니다.</div>
         </>
     );
 };
