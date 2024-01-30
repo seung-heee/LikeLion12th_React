146986d1d151a969994bdda5e42c53c7599f18b9
seung-heee <tmdgmldi2595@naver.com>
2024-01-31 02:00:52 +0900

    조건부 렌더링 삼항연산자 실습

--- src/component/conditionalRendering/GuestGreeting.js
+++ src/component/conditionalRendering/GuestGreeting.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GuestGreeting = () => {
+    const [user, setUser] = useState();
+
+    function handleChangeBtn(e) {
+        setUser(e.target.value)
+    }
+    
     return (
     <>
         <div>처음 방문하셨군요</div>
         <div>반갑습니다.</div>
+
+        <br/>
+        <div>
+            <p>계정이 있으신가요?</p>
+            <button value='Yes' onClick={handleChangeBtn}>Yes</button>
+            <button value='No' onClick={handleChangeBtn}>No</button>
+        </div>
+
+        {user == 'Yes' ? '로그인 하러가기' : '회원가입 하러가기' }
     </>
     );
 };
