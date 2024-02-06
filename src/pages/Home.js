a71a4cc26f758c265c5e9e35310b5d7a4fe20820
seung-heee <tmdgmldi2595@naver.com>
2024-02-06 13:16:16 +0900

    API 통신하기, GET

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState(null);
+    
+    const FetchUsers = async()=> {
+        try {
+            setUsers(null);
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.error('에러:', error);
+        }
+    }
+
+    
+    const AxiosUsers = async () => {
+        try {
+            setUsers(null);
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users'
+            );
+            setUsers(response.data); // 데이터는 response.data에 들어있음.
+            } catch (e) {
+            console.log('에러')
+            }
+      };
+
+
+
+    useEffect(() => {
+        AxiosUsers();
+        // FetchUsers();
+        
+    }, []);
+  
+    if (!users) return null;
+
     return (
-        <div>
-           <Link to="/project">프로젝트 페이지로 이동!</Link>
-        </div>
+        <>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} / {user.email}
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 };
 
