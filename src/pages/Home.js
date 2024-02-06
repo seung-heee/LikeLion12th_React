fde5f29be8cca635da6ab7d45e5e5e85de868b9d
seung-heee <tmdgmldi2595@naver.com>
2024-02-06 14:12:39 +0900

    API 통신하기, POST

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 
 const Home = () => {
     const [users, setUsers] = useState(null);
-    
+    const [posts, setPosts] = useState(null);
+
     const FetchUsers = async()=> {
         try {
             setUsers(null);
@@ -20,7 +21,6 @@ const Home = () => {
         }
     }
 
-    
     const AxiosUsers = async () => {
         try {
             setUsers(null);
@@ -33,15 +33,56 @@ const Home = () => {
             }
       };
 
+    const FetchPosts = async()=> {
+        try {
+            setPosts(null);
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                title: '새로운 포스트',
+                body: '포스트 내용',
+                userId: 1,
+                }),
+            });
+            
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            
+            const data = await response.json();
+            console.log('새로운 포스트가 생성되었습니다:', data);
+            setPosts(data);
+        } catch (error) {
+            console.error('에러:', error);
+        }
+    }
 
+    const AxoisPosts = async()=> {
+        try {
+            setPosts(null);
+            const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+                title: '새로운 포스트',
+                body: '포스트 내용',
+                userId: 1,
+            });
+
+            console.log('axios : 새로운 포스트가 생성되었습니다:', response.data);
+            setPosts(response.data);
+        } catch (error) {
+            console.error('에러:', error);
+        }
+    }
 
     useEffect(() => {
         AxiosUsers();
         // FetchUsers();
-        
     }, []);
   
     if (!users) return null;
+    if (!posts) return null;
 
     return (
         <>
